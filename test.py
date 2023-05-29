77b551830376629f579920d88fe3a2e5a6f0362c
tathagatankit <imtathagat@gmail.com>
2023-05-29 18:36:49 +0530

    commit to test deploy

--- test.py
+++ test.py
@@ -1,17 +1,39 @@
 import streamlit as st
+import redis
 
-placeholder = st.empty()
+# Create Redis connection
 
-# Replace the placeholder with some text:
-placeholder.text("Hello")
+# from database import get_redis_connection
+r = r = redis.Redis(host='localhost', port=6379, db=0)
 
-# Replace the text with a chart:
-placeholder.line_chart({"data": [1, 5, 2, 6]})
+def upload_pdf_file():
+    uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+    if uploaded_file is not None:
+        r.set(uploaded_file.name, uploaded_file.read())
+        st.success("File uploaded successfully")
 
-# Replace the chart with several elements:
-with placeholder.container():
-    st.write("This is one element")
-    st.write("This is another")
+def view_pdf_file():
+    keys = r.keys()
+    selected_file = st.selectbox("Select a file to view", keys)
+    if selected_file:
+        file_data = r.get(selected_file)
+        st.write(file_data)
+        st.download_button(
+            label="Download PDF",
+            data=file_data,
+            file_name=selected_file,
+            mime="application/pdf"
+        )
 
-# Clear all those elements:
-placeholder.empty()
+def main():
+    st.title("PDF File Uploader")
+    st.sidebar.header("Navigation")
+    pages = {
+        "Upload PDF file": upload_pdf_file,
+        "View PDF file": view_pdf_file,
+    }
+    page = st.sidebar.selectbox("Select a page", tuple(pages.keys()))
+    pages[page]()
+
+if __name__ == "__main__":
+    main()
